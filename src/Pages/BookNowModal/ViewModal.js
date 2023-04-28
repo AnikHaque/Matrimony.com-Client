bb5548576c183d7b6b6ecb9e867714a704421d7a
AnikHaque <ekramul15-811@diu.edu.bd>
2023-04-28 18:00:47 +0600

    profile added

--- src/Pages/BookNowModal/ViewModal.js
+++ src/Pages/BookNowModal/ViewModal.js
@@ -61,56 +61,276 @@ const ViewModal = ({ bookProduct, setBookProduct }) => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">Details:</h3>
-          <h3 className="text-lg">{name}</h3>
-          <form
-            onSubmit={handleModal}
-            className="grid grid-cols-1 gap-3 text-black p-5"
+          <h3 className="text-lg font-bold text-center">Details:</h3>
+          <h3 className="text-lg text-center">{name}</h3>
+          <p className="font-bold text-md">Short Description :</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+</p>
+<div className="mt-8">
+<label htmlFor="my-modal-5" className="bg-primary text-white w-full pl-32 pr-32 text-xl pt-4 pb-4 rounded-md m-1 font-bold border-black">Personal Details</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box h-full rounded-md">
+  <label
+            htmlFor="my-modal-5"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
           >
-            <input
-              type="text"
-              value={user?.displayName}
-              name="Username"
-              placeholder="User Name"
-              className="input w-full bg-white "
-            />
-            <input
-              type="email"
-              value={user?.email}
-              name="email"
-              placeholder="Email"
-              className="input w-full bg-white "
-            />
-            <input
-              type="text"
-              name="name"
-              value={name}
-              disabled
-              placeholder="Item Name"
-              className="input w-full bg-blue-100"
-            />
-            <input
-              type="text"
-              name="resalePrice"
-              value={resalePrice}
-              disabled
-              placeholder="Price"
-              className="input w-full bg-white"
-            />
-            <input
-              type="text"
-              name="Phone"
-              placeholder="Phone"
-              className="input w-full bg-white"
-            />
-            <input
-              type="text"
-              name="MeetingLocation"
-              placeholder="Meeting Location"
-              className="input w-full bg-white"
-            />
-            <input className="btn btn-primary" type="submit" value="Submit" />
-          </form>
+            ✕
+          </label>
+    <div className="grid grid-cols-2 lg:grid-cols-2 mt-10">
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Color:</b>
+      <p>White</p>
+      <b className="mb-2">Date of Birth:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      
+    </div>
+   
+  </div>
+</div>
+</div>
+<br></br>
+<div className="mt-8">
+<label htmlFor="my-modal-5" className="bg-primary text-white w-full pl-32 pr-32 text-xl pt-4 pb-4 rounded-md m-1 font-bold border-black">Family Details</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box h-full rounded-md">
+  <label
+            htmlFor="my-modal-5"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+    <div className="grid grid-cols-2 lg:grid-cols-2 mt-10">
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Color:</b>
+      <p>White</p>
+      <b className="mb-2">Date of Birth:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      
+    </div>
+   
+  </div>
+</div>
+</div>
+<br></br>
+<div className="mt-8">
+<label htmlFor="my-modal-5" className="bg-primary text-white w-full pl-28 pr-28 text-xl pt-4 pb-4 rounded-md m-1 font-bold border-black">Professional Details</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box h-full rounded-md">
+  <label
+            htmlFor="my-modal-5"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+    <div className="grid grid-cols-2 lg:grid-cols-2 mt-10">
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Color:</b>
+      <p>White</p>
+      <b className="mb-2">Date of Birth:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      
+    </div>
+   
+  </div>
+</div>
+</div>
+<br></br>
+<div className="mt-8">
+<label htmlFor="my-modal-5" className="bg-primary text-white w-full pl-28 pr-28 text-xl pt-4 pb-4 rounded-md m-1 font-bold border-black">Educational Details</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box h-full rounded-md">
+  <label
+            htmlFor="my-modal-5"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+    <div className="grid grid-cols-2 lg:grid-cols-2 mt-10">
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Color:</b>
+      <p>White</p>
+      <b className="mb-2">Date of Birth:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      
+    </div>
+   
+  </div>
+</div>
+</div>
+<br></br>
+<div className="mt-8">
+<label htmlFor="my-modal-5" className="bg-primary text-white w-full pl-32 pr-32 text-xl pt-4 pb-4 rounded-md m-1 font-bold border-black">Other Details</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box h-full rounded-md">
+  <label
+            htmlFor="my-modal-5"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+    <div className="grid grid-cols-2 lg:grid-cols-2 mt-10">
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Color:</b>
+      <p>White</p>
+      <b className="mb-2">Date of Birth:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      <b className="mb-2">Name:</b>
+      <p>{user.displayName}</p>
+      <b className="mb-2">Email:</b>
+      <p>{user.email}</p>
+      <b className="mb-2">Age:</b>
+      <p>26</p>
+      <b className="mb-2">Nationality:</b>
+      <p>Bangladeshi</p>
+      
+    </div>
+   
+  </div>
+</div>
+</div>
+
         </div>
       </div>
     </div>
