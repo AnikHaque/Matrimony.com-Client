27180d370c8c4f2bee54f9df4c6dc452ea4fee17
AnikHaque <ekramul15-811@diu.edu.bd>
2023-05-14 02:08:42 +0600

    Top Profile full feature done with payment

--- src/Pages/profile/Profile.js
+++ src/Pages/profile/Profile.js
@@ -13,10 +13,48 @@ import { useLoaderData, useParams } from 'react-router-dom'
 import { useGetTopProfileByIdQuery } from '../../features/topProfile/topProfileApi'
 
 const Profile = () => {
-  const {  user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const {id} = useParams();
 const {data} = useGetTopProfileByIdQuery(id);
- const {name, designation, email, img, description} = data || {}
+ const {_id, name, designation, email, img, description, phone, presentlocation, age} = data || {}
+
+ const handlePlaceOrder = (event,data) => {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const customer = user.displayName;
+  const email = user?.email || "unregistered";
+  const phone = form.phone.value;
+  const address = form.address.value;
+  const age = form.age.value;
+  const currency = form.currency.value;
+
+  const order = {
+    service:_id,
+    name,
+    age,
+    customer,
+    email,
+    phone,
+    address,
+    currency
+  };
+
+  fetch("http://localhost:5000/booktopprofile", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+     
+    },
+    body: JSON.stringify(order)
+  })
+    .then((res) => res.json())
+    .then((data) => {
+  window.location.replace(data.url);
+    })
+    .catch((er) => console.error(er));
+};
+  
   return (
    <div>
     <NavBar></NavBar>
@@ -58,7 +96,82 @@ const {data} = useGetTopProfileByIdQuery(id);
               <li><a href="">groups</a></li>
               <li><a href="">about</a></li>
             </ul>
-            <button className='btn btn-primary'>Take Appointment</button>
+            {/* The button to open modal */}
+<label htmlFor="my-modal-3" className="btn btn-primary">Take Appointment</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my-modal-3" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box relative">
+    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+    <form onSubmit={handlePlaceOrder} className="">
+       
+       <div>
+         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+           <input
+             name="name"
+             type="text"
+             placeholder="Name"
+             className="input input-ghost w-full  input-bordered"
+        defaultValue={user.displayName}
+           />
+           <input
+             name="cusname"
+             type="text"
+             placeholder="Customer Name"
+             className="input input-ghost w-full  input-bordered"
+             defaultValue={name}
+           />
+         
+           <input
+             name="phone"
+             type="text"
+             placeholder="Your Phone"
+             className="input input-ghost w-full  input-bordered"
+             defaultValue={phone}
+             required
+           />
+           <input
+             name="email"
+             type="text"
+             placeholder="Your email"
+          defaultValue={email}
+             className="input input-ghost w-full  input-bordered"
+             readOnly
+           />
+         <select
+           defaultValue="BDT"
+           name="currency"
+           className="select select-bordered max-w-xs"
+         >
+           <option value="BDT">BDT</option>
+           <option value="USD">USD</option>
+         </select>
+
+         <input
+           type="number"
+           name="age"
+           placeholder="Type 1500 Taka to pay"
+           className="input input-ghost w-full  input-bordered"
+           defaultValue={age}
+         />
+         </div>
+
+
+         <textarea
+           name="address"
+           className="textarea textarea-bordered h-24 w-full my-5"
+           placeholder="Your Address"
+           defaultValue={presentlocation}
+           required
+         ></textarea>
+
+         <input className="btn btn-secondary w-full" type="submit" value="Confirm Payment for Appointment" />
+       </div>
+     </form>
+  </div>
+</div>
           </nav>
 
           <div class="photos">
